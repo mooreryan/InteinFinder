fa65aa7ef59c6d07d392bd9220a40fdc54391a21
Ryan M. Moore <rmm1047@gmail.com>
2022-12-11 19:55:21 -0500

    Remove unused functions and test

--- lib/zero_indexed_int.ml
+++ lib/zero_indexed_int.ml
@@ -13,13 +13,9 @@ include Comparable.Make_plain (T)
 
 let of_zero_indexed_int i = i
 
-let of_one_indexed_int i = i - 1
-
 let to_zero_indexed_int i = i
 
 let to_one_indexed_int i = i + 1
-
-let to_zero_indexed_string t = Int.to_string @@ to_zero_indexed_int t
 
 let to_one_indexed_string t = Int.to_string @@ to_one_indexed_int t
 
@@ -29,3 +25,14 @@ let assert_positive_or_zero t =
   let i = to_zero_indexed_int t in
   if i < 0 then Or_error.errorf "Expected index >= 0, but got %d" i
   else Or_error.return ()
+
+let%expect_test "assert_positive_or_zero" =
+  print_s @@ [%sexp_of: unit Or_error.t] @@ assert_positive_or_zero
+  @@ of_zero_indexed_int (-1) ;
+  [%expect {| (Error "Expected index >= 0, but got -1") |}] ;
+  print_s @@ [%sexp_of: unit Or_error.t] @@ assert_positive_or_zero
+  @@ of_zero_indexed_int 0 ;
+  [%expect {| (Ok ()) |}] ;
+  print_s @@ [%sexp_of: unit Or_error.t] @@ assert_positive_or_zero
+  @@ of_zero_indexed_int 1 ;
+  [%expect {| (Ok ()) |}]
--- lib/zero_indexed_int.mli
+++ lib/zero_indexed_int.mli
@@ -4,13 +4,9 @@ type t [@@deriving sexp_of]
 
 val of_zero_indexed_int : int -> t
 
-val of_one_indexed_int : int -> t
-
 val to_zero_indexed_int : t -> int
 
 val to_one_indexed_int : t -> int
-
-val to_zero_indexed_string : t -> string
 
 val to_one_indexed_string : t -> string
 
