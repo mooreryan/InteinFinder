f25227c92325793faf04ecc9d5c33a1b28314e82
Ryan M. Moore <rmm1047@gmail.com>
2022-12-12 14:44:03 -0500

    Pass dir as a parameter

--- lib/out_file_name.ml
+++ lib/out_file_name.ml
@@ -8,7 +8,7 @@ let intein_hit_checks dir = dir ^/ "3_intein_hit_checks.tsv"
 
 let mmseqs_search_out dir = dir ^/ "1_mmseqs_search_out.tsv"
 
-let mmseqs_search_summary = "2_mmseqs_search_summary.tsv"
+let mmseqs_search_summary dir = dir ^/ "2_mmseqs_search_summary.tsv"
 
 let pipeline_info dir = dir ^/ "2_pipeline_info.txt"
 
@@ -16,6 +16,6 @@ let putative_intein_regions dir = dir ^/ "1_putative_intein_regions.tsv"
 
 let rpsblast_search_out dir = dir ^/ "1_rpsblast_search_out.tsv"
 
-let rpsblast_search_summary = "2_rpsblast_search_summary.tsv"
+let rpsblast_search_summary dir = dir ^/ "2_rpsblast_search_summary.tsv"
 
 let trimmed_inteins dir = dir ^/ "4_trimmed_inteins.faa"
