5bbb47ae41ca64a0f73eb23d63b4d71b4a1abf5a
Ryan M. Moore <rmm1047@gmail.com>
2023-01-16 21:44:28 -0500

    Clean up

--- lib/tier.ml
+++ lib/tier.ml
@@ -144,7 +144,7 @@ module Map = struct
     Or_error.try_with (fun () -> Otoml.Parser.from_string s)
 
   (* Validate that start residue key is good and return Or_error. *)
-  let start_residue_key s =
+  let single_residue_key s =
     if String.length s = 1 then Or_error.return @@ String.uppercase s
     else Or_error.errorf "expected key to be a single residue but got '%s'" s
 
@@ -153,13 +153,12 @@ module Map = struct
     if String.length s = 2 then Or_error.return @@ String.uppercase s
     else Or_error.errorf "expected key to be two end residues but got '%s'" s
 
-  (* Parse the (string * string) list to the Map *)
-  let start_residue_parser l =
+  let residue_parser key_validator l =
     let open Or_error.Let_syntax in
     let residue_tier_list =
       List.map l ~f:(fun (k, v) ->
           let%bind tier = create v in
-          let%map k = start_residue_key k in
+          let%map k = key_validator k in
           (k, tier) )
     in
     let%bind residue_tier_list = Or_error.all residue_tier_list in
@@ -169,37 +168,20 @@ module Map = struct
     let%bind map = of_alist_or_error residue_tier_list in
     return map
 
-  (* Parse the (string * string) list to the Map *)
-  let end_residues_parser l =
-    let open Or_error.Let_syntax in
-    let residue_tier_list =
-      List.map l ~f:(fun (k, v) ->
-          let%bind tier = create v in
-          let%map k = end_residues_key k in
-          (k, tier) )
-    in
-    let%bind residue_tier_list = Or_error.all residue_tier_list in
-    (* Now we check that the tiers are valid *)
-    let tier_list = List.map residue_tier_list ~f:snd in
-    let%bind _tier_list = Valid_list.create tier_list in
-    let%bind map = of_alist_or_error residue_tier_list in
-    return map
-
-  let tiny_toml_start_residue_converter =
+  let tiny_toml_converter residue_parser =
     let open Tiny_toml in
     let acc = Otoml.get_table_values Otoml.get_string in
-    Converter.v acc start_residue_parser
-
-  let tiny_toml_end_residues_converter =
-    let open Tiny_toml in
-    let acc = Otoml.get_table_values Otoml.get_string in
-    Converter.v acc end_residues_parser
+    Converter.v acc residue_parser
 
   let tiny_toml_single_residue_term ~default path =
-    Tiny_toml.Value.find_or ~default path tiny_toml_start_residue_converter
+    Tiny_toml.Value.find_or ~default path
+    @@ tiny_toml_converter
+    @@ residue_parser single_residue_key
 
   let tiny_toml_end_residues_term ~default path =
-    Tiny_toml.Value.find_or ~default path tiny_toml_end_residues_converter
+    Tiny_toml.Value.find_or ~default path
+    @@ tiny_toml_converter
+    @@ residue_parser end_residues_key
 
   let find : t -> string -> Tier_or_fail.t =
    fun t element ->
